35ef9ff9f5b48e74fd79dd32160a952909833c4f
jo314652 <jo314652@gmail.com>
2017-11-20 23:14:27 +0800

    stv

---
+++ 6-11B/source/Source.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 10
+
+int main(void)
+{
+	int a[SIZE] = { 2, 6, 4, 8, 10, 12, 89, 68, 45, 37 };
+	int pass, hold;
+	size_t i;
+
+	puts("Data items in original order");
+
+	for (i = 0; i < SIZE; ++i)
+	{
+		printf("%4d", a[i]);
+	}
+
+	for (pass = 1; pass < SIZE; ++pass)
+	{
+		bool stv = false;
+		for (i = pass-1; i < SIZE - 1; ++i)
+		{
+			if (a[i]>a[i + 1])
+			{
+				hold = a[i];
+				a[i] = a[i + 1];
+				a[i + 1] = hold;
+				stv = true;
+			}
+
+		}
+		if (!stv) break;
+	}
+
+	puts("\nData items in ascending order");
+
+	for (i = 0; i < SIZE; ++i)
+	{
+		printf("%4d", a[i]);
+	}
+
+	puts("");
+
+	system("pause");
+	return 0;
+}
